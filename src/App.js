f9aa2e1e0c79b454a844cb2d9ec7b218e20bb6cf
William Pinto <williamporto_14@hotmail.com>
2022-04-24 12:26:10 -0300

    Making App.js component statefull and alowing to select the desired currency

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import Books from './components/Books';
 import styles from './App.module.scss';
 import CurrencyContext from './context/currency';
 
+import { useState } from 'react';
+
 function App() {
+  const [currency, setCurrency] = useState('R$');
+
+  const handleChangeCurrency = (currency) => {
+    setCurrency(currency);
+  }
+
   return (
-    <CurrencyContext.Provider value="R$">
+    <CurrencyContext.Provider value={currency}>
       <section className={styles.App}>
         <h1>My Books</h1>
+        <section className={styles.CurrencySelection}>
+          <button onClick={() => handleChangeCurrency('€')}>EUR</button>
+          <button onClick={() => handleChangeCurrency('$')}>DÓLAR</button>
+          <button onClick={() => handleChangeCurrency('R$')}>REAIS</button>
+        </section>
         <Books list={getBooksData()} />
       </section>
     </CurrencyContext.Provider>
