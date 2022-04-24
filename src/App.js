aeec35243c9e6113fce224b73126cb9304431a3d
William Pinto <williamporto_14@hotmail.com>
2022-04-24 12:34:55 -0300

    Getting currencies from API and mapping it to a list of buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import {getBooksData} from './services/book-api';
+import { useState } from "react";
+import { getBooksData } from "./services/book-api";
+import { getCurrencies } from "./services/currencies-api";
 
-import Books from './components/Books';
-import styles from './App.module.scss';
-import CurrencyContext from './context/currency';
-
-import { useState } from 'react';
+import Books from "./components/Books";
+import styles from "./App.module.scss";
+import CurrencyContext from "./context/currency";
 
 function App() {
-  const [currency, setCurrency] = useState('R$');
+  const [currency, setCurrency] = useState("R$");
 
   const handleChangeCurrency = (currency) => {
     setCurrency(currency);
-  }
-
+  };
+  
   return (
     <CurrencyContext.Provider value={currency}>
       <section className={styles.App}>
         <h1>My Books</h1>
         <section className={styles.CurrencySelection}>
-          <button onClick={() => handleChangeCurrency('€')}>EUR</button>
-          <button onClick={() => handleChangeCurrency('$')}>DÓLAR</button>
-          <button onClick={() => handleChangeCurrency('R$')}>REAIS</button>
+          {Object.values(getCurrencies()).map(item => (
+            <button onClick={() => handleChangeCurrency(item.symbol)}>{item.label}</button>
+          ))}
         </section>
         <Books list={getBooksData()} />
       </section>
---
+++ src/services/currencies-api.js
@@ -0,0 +1,16 @@
+export const getCurrencies = () => {
+    return {
+        Euro: {
+            symbol: '€',
+            label: 'Euro',
+        },
+        Usd: {
+            symbol: '$',
+            label: 'US Dollar',
+        },
+        Real: {
+            symbol: 'R$',
+            label:  'Brazilian Reais',
+        },
+    }
+  };
